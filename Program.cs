97e15b59022801549633407f20d1b25a34025114
Anna-312 <anna-062003@mail.ru>
2024-06-13 16:09:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool entered = false;
+            bool coped = false;
+            Table table = new Table();
+            Table table1 = new Table();
+            void start()
+            {
+                int num;
+                Console.WriteLine("Выберите действие:\n1 - Ввести таблицу\n2 - Вывести таблицу\n3 - Добавить строку\n4 - Добавить столбец\n5 - Добавить элемент\n6 - Исключить строку\n7 - Исключить столбец\n8 - Скопировать таблицу\n9 - Выход");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num == 1)
+                    input();
+                else
+                    if (num == 2)
+                    output();
+                else
+                        if (num == 3)
+                    addRow();
+                else
+                            if (num == 4)
+                    addCol();
+                else
+                                if (num == 5)
+                    addElem();
+                else
+                                    if (num == 6)
+                    removeRow();
+                else
+                    if (num == 7)
+                    removeCol();
+                else
+                    if (num == 8)
+                    copyTable();
+            }
+            void input()
+            {
+                int m, n, k;
+                string name, info;
+
+                if (entered)
+                    table.Clear();
+                entered = true;
+                if (coped)
+                    table1.Clear();
+                coped = false;
+                Console.WriteLine("Введите количество строк:");
+                m = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < m; i++)
+                {
+                    Console.WriteLine("Введите название строки:");
+                    name = Console.ReadLine();
+                    table.AddRow(name);
+                }
+                Console.WriteLine("Введите количество стoлбцов:");
+                n = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Введите название столбца:");
+                    name = Console.ReadLine();
+                    table.AddCol(name);
+                }
+                Console.WriteLine("Введите количество элементов:");
+                k = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < k; i++)
+                {
+                    int a, b;
+                    Console.WriteLine("Введите номер строки элемента:");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите номер столбца элемента:");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите содержание элемента:");
+                    info = Console.ReadLine();
+                    table.AddElem(info, a, b);
+                }
+                start();
+            }
+
+            void output()
+            {
+                if (entered)
+                    table.Output();
+                else
+                    Console.WriteLine("Таблица не введена!");
+                if (coped)
+                {
+                    Console.WriteLine("Скопированная таблица:");
+                    table1.Output();
+                }
+                start();
+            }
+            void addRow()
+            {
+                if (!entered)
+                    Console.WriteLine("Таблица не введена!");
+                else
+                {
+                    string name;
+                    Console.WriteLine("Введите название строки:");
+                    name = Console.ReadLine();
+                    table.AddRow(name);
+                }
+                start();
+            }
+            void addCol()
+            {
+                if (!entered)
+                    Console.WriteLine("Таблица не введена!");
+                else
+                {
+                    string name;
+
+                    Console.WriteLine("Введите название столбца:");
+                    name = Console.ReadLine();
+                    table.AddCol(name);
+                }
+                start();
+            }
+            void addElem()
+            {
+                if (!entered)
+                {
+                    Console.WriteLine("Таблица не введена!");
+                }
+                else
+                {
+                    string info;
+                    int a, b;
+                    Console.WriteLine("Введите номер строки элемента:");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите номер столбца элемента:");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите содержимое элемента:");
+                    info = Console.ReadLine();
+                    table.AddElem(info, a, b);
+                }
+                start();
+            }
+            void removeRow()
+            {
+                if (!entered)
+                    Console.WriteLine("Таблица не введена!");
+                else
+                {
+                    int n;
+                    Console.WriteLine("Введите номер строки:");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    table.RemoveRow(n);
+                }
+                start();
+            }
+
+            void removeCol()
+            {
+                if (!entered)
+                    Console.WriteLine("Таблица не введена!");
+                else
+                {
+                    int n;
+                    Console.WriteLine("Введите номер столбца:");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    table.RemoveCol(n);
+                }
+                start();
+            }
+            void copyTable()
+            {
+                if (entered)
+                {
+                    table1.Copy(table);
+                    coped = true;
+                }
+                else
+                    Console.WriteLine("Таблица не введена!");
+                start();
+            }
+            start();
+        }
+    }
+}
---
+++ Table.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Node
+    {
+        public string name;
+        public Node next;
+        public int number;
+        public Node(string nm)
+        {
+            name = nm;
+            next = null;
+        }
+    }
+    class Base
+    {
+        protected Node first;
+        public Base()
+        {
+            first = null;
+        }
+        public void Add(Node node)
+        {
+            Node cur;
+            cur = first;
+            if (cur == null)
+            {
+                first = node;
+                first.number = 0;
+            }
+            else
+            {
+                while (cur.next != null)
+                    cur = cur.next;
+                cur.next = node;
+                cur.next.number = cur.number + 1;
+            }
+        }
+        public void Remove(Node node)
+        {
+            Node cur;
+            cur = first;
+            if (first == null)
+                return;
+            if (first == node)
+            {
+                first = node.next;
+                return;
+            }
+            while (cur.next != node)
+                cur = cur.next;
+            cur.next = cur.next.next;
+            while (cur.next != null)
+            {
+                cur.next.number = cur.number + 1;
+                cur = cur.next;
+            }
+        }
+        public Node Find(string nm)
+        {
+            Node cur;
+            cur = first;
+            while (cur.name != nm)
+                cur = cur.next;
+            return cur;
+        }
+        public Node this[int i]
+        {
+            get
+            {
+                Node cur;
+                cur = first;
+                while (cur.number != i)
+                    cur = cur.next;
+                return cur;
+            }
+        }
+        public int Length()
+        {
+            Node cur;
+            int k;
+            k = 0;
+            cur = first;
+            while (cur != null)
+            {
+                k += 1;
+                cur = cur.next;
+            }
+            return k;
+        }
+        public void Clear()
+        {
+            first = null;
+        }
+    }
+    class Rows : Base
+    {
+        public Rows()
+        {
+            first = null;
+        }
+        public void Copy(Rows rows1)
+        {
+            Node cur, node;
+            cur = rows1.first;
+            while (cur != null)
+            {
+                node = new Node(cur.name);
+                Add(node);
+                cur = cur.next;
+            }
+        }
+    }
+    class Cols : Base
+    {
+        public Cols()
+        {
+            first = null;
+        }
+        public void Copy(Cols cols1)
+        {
+            Node cur, node;
+            cur = cols1.first;
+            while (cur != null)
+            {
+                node = new Node(cur.name);
+                Add(node);
+                cur = cur.next;
+            }
+        }
+
+    }
+    class Elem
+    {
+        public string inf;
+        public int row;
+        public int column;
+        public Elem next;
+        public Elem(string info, int r, int c)
+        {
+            inf = info;
+            row = r;
+            column = c;
+            next = null;
+        }
+    }
+
+    class Elems
+    {
+        Elem first;
+        public Elems()
+        {
+            first = null;
+        }
+
+        public void Clear()
+        {
+            first = null;
+        }
+        public void Add(Elem elem)
+        {
+            Elem cur;
+            cur = first;
+            if (cur == null)
+                first = elem;
+            else
+            {
+                while (cur.next != null)
+                    cur = cur.next;
+                cur.next = elem;
+            }
+        }
+
+        public void Copy(Elems elems1)
+        {
+            Elem cur, elem;
+            cur = elems1.first;
+            while (cur != null)
+            {
+                elem = new Elem(cur.inf, cur.row, cur.column);
+                Add(elem);
+                cur = cur.next;
+            }
+        }
+
+        public Elem this[int i, int j]
+        {
+            get
+            {
+                Elem cur;
+                cur = first;
+                while (cur != null)
+                {
+                    if ((cur.row == i) && (cur.column == j))
+                        return cur;
+                    else
+                        cur = cur.next;
+                }
+                return null;
+            }
+        }
+        public void Dell(Elem elem)
+        {
+            Elem cur;
+            cur = first;
+            if (cur == null)
+                return;
+            if (first == elem)
+            {
+                first = elem.next;
+                return;
+            }
+            while (cur.next != elem)
+                cur = cur.next;
+            cur.next = cur.next.next;
+        }
+        public void DellAllRow(int i)
+        {
+            Elem cur;
+            cur = first;
+            while (cur != null)
+            {
+                if (cur.row == i)
+                    Dell(cur);
+                else
+                    if (cur.row > i)
+                    cur.row -= 1;
+                cur = cur.next;
+            }
+        }
+        public void DellAllCol(int j)
+        {
+            Elem cur;
+            cur = first;
+            while (cur != null)
+            {
+                if (cur.column == j)
+                    Dell(cur);
+                else
+                    if (cur.column > j)
+                    cur.column -= 1;
+                cur = cur.next;
+            }
+        }
+    }
+    class Table
+    {
+        Rows rows;
+        Cols cols;
+        Elems elems;
+        public Table()
+        {
+            rows = new Rows();
+            cols = new Cols();
+            elems = new Elems();
+        }
+        public void AddRow(string name)
+        {
+            Node node = new Node(name);
+            rows.Add(node);
+        }
+        public void AddCol(string name)
+        {
+            Node node = new Node(name);
+            cols.Add(node);
+        }
+        public void AddElem(string info, int numRow, int numCol)
+        {
+            Elem elem = new Elem(info, numRow, numCol);
+            elems.Add(elem);
+        }
+        public void Copy(Table table1)
+        {
+            rows.Copy(table1.rows);
+            cols.Copy(table1.cols);
+            elems.Copy(table1.elems);
+        }
+        public void RemoveRow(int i)
+        {
+            Node node;
+            node = rows[i];
+            elems.DellAllRow(i);
+            rows.Remove(node);
+        }
+        public void RemoveCol(int i)
+        {
+            Node node;
+            node = cols[i];
+            elems.DellAllCol(i);
+            cols.Remove(node);
+        }
+        public void Output()
+        {
+            Elem elem;
+            int n, m;
+            Console.WriteLine();
+            n = rows.Length();
+            m = cols.Length();
+            Console.Write("{0, 20}", "-");
+            for (int i = 0; i < m; i++)
+                Console.Write("{0, 20}", cols[i].name);
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0, 20}", rows[i].name);
+                for (int j = 0; j < m; j++)
+                {
+                    elem = elems[i, j];
+                    if (elem != null)
+                        Console.Write("{0, 20}", elem.inf);
+                    else
+                        Console.Write("{0, 20}", "-");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        public void Clear()
+        {
+            rows.Clear();
+            cols.Clear();
+            elems.Clear();
+        }
+    }
+}
